f52f159622be6a0cdee1f52b985b89efb7c1b182
Farhankaioum <km1472233@gmail.com>
2021-02-16 12:00:44 +0600

    problem solved

--- pages/checkout.js
+++ pages/checkout.js
@@ -45,6 +45,14 @@ const useStyles = makeStyles((theme) => ({
         letterSpacing: '1px',
         textTransform: 'capitalize'
     },
+    noProduct: {
+        textAlign: 'center',
+        letterSpacing: '1px',
+        textTransform: 'uppercase',
+        color: 'red',
+        minHeight: '80vh',
+
+    },
     title: {
         marginTop: '30px',
         letterSpacing: '1px',
@@ -58,7 +66,7 @@ const Checkout = () => {
 
     const [ cart, setCart ] = useContext( AppContext );
 
-    let carts = localStorageData('shopping-cart')
+    let carts = localStorageData('shopping-cart');
 
     // order posting method
     const orderHandler = async event => {
@@ -115,53 +123,68 @@ const Checkout = () => {
         <div>
             <Grid container spacing={3}>
                 <Grid item md={7}>
-                    <h2 className={classes.message}>Provide your information to confirm order</h2>
-                    <Paper className={classes.paper}>
+                    {
+                        Object.keys(carts).length !== 0  && 
+                    <>
+                        <h2 className={classes.message}>Provide your information to confirm order</h2>
+                        <Paper className={classes.paper}>
 
-                        <form className={classes.form} onSubmit={orderHandler}>
-                            <div>
-                                <TextField
-                                    id="name"
-                                    label="Name"
-                                    variant="outlined"
-                                    name="name"
-                                    required
-                                />
-                            </div>
-                            <div>
-                                <TextField
-                                    id="phone"
-                                    name="phone"
-                                    label="Phone"
-                                    variant="outlined"
-                                    required
-                                />
-                            </div>
-                            <div>
-                                <TextField
-                                    id="address"
-                                    name="address"
-                                    label="Address"
-                                    variant="outlined"
-                                    required
-                                />
-                            </div>
-                            <div style={{ margin: "15px 0px 10px 0px"}}>
+                            <form className={classes.form} onSubmit={orderHandler}>
+                                <div>
+                                    <TextField
+                                        id="name"
+                                        label="Name"
+                                        variant="outlined"
+                                        name="name"
+                                        required
+                                    />
+                                </div>
+                                <div>
+                                    <TextField
+                                        id="phone"
+                                        name="phone"
+                                        label="Phone"
+                                        variant="outlined"
+                                        required
+                                    />
+                                </div>
+                                <div>
+                                    <TextField
+                                        id="address"
+                                        name="address"
+                                        label="Address"
+                                        variant="outlined"
+                                        required
+                                    />
+                                </div>
+                                <div style={{ margin: "15px 0px 10px 0px"}}>
+                                
                                 <Button
                                     type="submit"
                                     variant="contained"
                                     color="primary">
                                     Place Confirm
                                 </Button>
-                            </div>
-                        </form>
-                    </Paper>
+                                
+                                </div>
+                            </form>
+                        </Paper>
+                    </>
+                    }
+
+                    {
+                        Object.keys(carts).length == 0  && 
+                        <div className={classes.noProduct}>
+                           <h2>You have no products inside your cart to checkout</h2> 
+                        </div>
+                    }
+                    
                 </Grid>
 
                 <Grid item md={4}>
-                    <h3 className={classes.title}>Product Information</h3>
                     { Object.keys(carts).length !== 0  && 
                         <>
+                        <h3 className={classes.title}>Product Information</h3>
                         <TableContainer component={Paper} className={classes.table}>
                             <Table  aria-label="simple table">
                                 <TableHead>
